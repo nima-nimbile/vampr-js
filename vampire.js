92539db45545cafb9107fd06188be2f1a6758629
Nima <nima.mohamed@hotmail.com>
2023-02-25 16:59:04 -0800

    W3 D5

--- vampire.js
+++ vampire.js
@@ -10,22 +10,41 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentvampire = this;
+    while (currentvampire.creator) {
+      currentvampire = currentvampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if(this.offspring === null) {
+      return false;
+  };
+    for(let i = 0; i < this.numberOfOffspring; i++){
+      if(vampire.name === this.offspring[i].name)
+        return true;
+    }
 
+    return false;
   }
 
   /** Stretch **/
@@ -39,6 +58,19 @@ class Vampire {
 
   }
 }
+const original = new Vampire("Original", 6);
+const ansel = new Vampire("Ansel", 5);
+const bart = new Vampire("Bart", 4);
+const elgort = new Vampire("Elgort", 3);
+const sarah = new Vampire("Sarah", 2);
+const andrew = new Vampire("Andrew", 1);
+original.addOffspring(ansel);
+original.addOffspring(bart);
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+elgort.addOffspring(andrew);
+console.log(original.isMoreSeniorThan(original));
+
 
 module.exports = Vampire;
 
